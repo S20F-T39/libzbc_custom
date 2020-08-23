52b7856d717de5576cf6a49db83b7d80c3bc9821
wooyoung <djdj7087@kakao.com>
2020-08-23 11:13:27 +0900

    packaging

--- README.md
+++ README.md
@@ -2,14 +2,25 @@
 
 libzbc를 커스텀하여 파일 입출력을 수행합니다.
 
-> write 시에 zone 을 지정하는 방법
+> `write` 시에 `zone` 을 지정하는 방법
 1. `Conventional Zone` 은 일단 건드리지 않는다. `Conventional zone`을 무시한 채로 진행.
 2. `Empty Zone` 의 첫번째를 구하여 `Write`
 3. `Empty Zone` 이 없고 `Implicit Open` 이 있을 때에는 
 그 `Implicit Open Zone` 의 `Write Pointer` 를 구하여
-해당 `Write Pointer` 부터 작성
+해당 `Write Pointer` 부터 작성합니다.
 
-> zbc_list_zones 함수
+> `zbc_list_zones` 함수
 1. `empty_zones` 변수에 상태가 `Empty` 인 zone 담음 (배열이라고 생각하면 간단?)
-2. nr_zone 변수에 zone 개수를 지정해 준다.
+2. nr_zone 변수에 zone 개수를 지정해 줍니다.
 
+> `buf_size` 변수
+1. `buf_size` 변수는 I/O 시의 Block Size 를 지칭하는 것으로 판단되었습니다.
+2. `buf_size` 변수를 이미 시작할 때 512B 로 가정하고 진행하였습니다.
+
+> `write` 단계
+1. `write` 단계에서, `vector` 단위 I/O 는 고려하지 않았습니다.
+2. `vector` 단위의 I/O 를 고려하지 않았기 때문에, `io_size` 와 `buf_size` 가 512B 로 같습니다.
+
+> `file write` 단계
+1. 파일 입력이 `argument` 에서 강제되고 있기 때문에, `file I/O` 가 필수적입니다.
+2. `file size` 는 기본적으로 `Byte` 단위로 표현됩니다.
--- write/custom_write_zone.c
+++ write/custom_write_zone.c
@@ -54,8 +54,13 @@ int main(int argc, char **argv) {
 
     int init_zone_idx, zone_idx, i, fd = -1;
     int flags = O_RDWR;
-    char *path, *file = NULL;
+    char *path, *end, *file = NULL;
+    void *io_buffer = NULL;
+
     ssize_t ret = 1;
+    size_t io_size, io_align, buf_size = 512;
+
+    struct stat st;
 
     struct zbc_device *dev = NULL;
     struct zbc_device_info info = {0};
@@ -65,6 +70,8 @@ int main(int argc, char **argv) {
     struct zbc_zone *io_zone = NULL;
 
     unsigned int nr_zones;              // Number of Zones
+    unsigned long pattern = 0;
+    unsigned long long f_size;
 
     if (argc < 3) {
         usage:
@@ -107,7 +114,7 @@ int main(int argc, char **argv) {
     ret = zbc_open(path, flags, &dev);
     if (ret != 0) {
         if (ret == -ENODEV)
-            fprintf(stderr,"Open %s failed (not a zoned block device)\n",
+            fprintf(stderr, "Open %s failed (not a zoned block device)\n",
                     path);
         else
             fprintf(stderr, "Open %s failed (%s)\n",
@@ -131,7 +138,14 @@ int main(int argc, char **argv) {
 
     /* Get Target Zone */
     /* Conventional zone 을 제외하고 받아왔으므로, Conventional check 불필요 */
+    /* Empty zone 중에 제일 첫번째 zone */
     io_zone = &empty_zones[0];
+    zone_idx = (int) (io_zone->zbz_start / zbc_zone_length(io_zone));
+    if (!zbc_zone_sequential(io_zone)) {
+        errno = EINVAL;
+        perror("Invalid or Cannot find zone\n");
+        return -1;
+    }
 
     printf("Target zone: Zone %d / %d, type 0x%x (%s), "
            "cond 0x%x (%s), rwp %d, non_seq %d, "
@@ -149,12 +163,73 @@ int main(int argc, char **argv) {
            zbc_zone_wp(io_zone));
 
     /* Part of File I/O */
+    /**
+     * Check I/O alignment and get an I/O buffer
+     */
+    if (zbc_zone_sequential_req(io_zone))
+        io_align = info.zbd_pblock_size;        // Physical Block Size
+    else
+        io_align = info.zbd_lblock_size;        // Logical Block Size
+
+    if (buf_size % io_align) {
+        fprintf(stderr, "Invalid I/O size %zu (must me aligned on %zu)\n",
+                buf_size, io_align);
+        ret = 1;
+        goto out;
+    }
+
+    io_size = buf_size;
+    ret = posix_memalign((void **) &io_buffer, sysconf(_SC_PAGESIZE), io_size);
+    if (ret != 0) {
+        fprintf(stderr, "No memory for I/O buffer (%zu B)\n", io_size);
+        ret = 1;
+        goto out;
+    }
+
+    memset(io_buffer, pattern, io_size);
+
+    /* Open the file to read */
+    if (file) {
+        fd = open(file, O_LARGEFILE | O_RDONLY);
+        if (fd < 0) {
+            fprintf(stderr, "Open file \"%s\" failed %d (%s)\n",
+                    file, errno, strerror(errno));
+            ret = 1;
+            goto out;
+        }
+
+        ret = fstat(fd, &st);
+        if (ret != 0) {
+            fprintf(stderr, "Stat file \"%s\" failed %d (%s)\n",
+                    file, errno, strerror(errno));
+            ret = 1;
+            goto out;
+        }
+
+        if (S_ISREG(st.st_mode)) {
+            f_size = st.st_size;
+            printf("Valid File Size: %llu\n", f_size);
+        } else if (S_ISBLK(st.st_mode)) {
+            ret = ioctl(fd, BLKGETSIZE64, &f_size);
+            if (ret != 0) {
+                fprintf(stderr, "ioctl BLKGETSIZE64 block device \"%s\" failed %d (%s)\n",
+                        file, errno, strerror(errno));
+                ret = 1;
+                goto out;
+            }
+        } else {
+            fprintf(stderr, "Unsupported file \"%s\" type\n", file);
+            ret = 1;
+            goto out;
+        }
+    }
 
     out:
     if (fd > 0)
         close(fd);
     zbc_close(dev);
     free(empty_zones);
+    free(io_buffer);
 
     return ret;
 }

f4f1236fe3675938d83670e5a57998af215ff0aa
wooyoung <djdj7087@kakao.com>
2020-08-23 15:35:02 +0900

    0823 add header file

---
+++ custom_zbc.h
@@ -0,0 +1,10 @@
+//
+// Created by wooyoung on 20. 8. 23..
+//
+
+#ifndef LIBZBCCUSTOM_CUSTOM_ZBC_H
+#define LIBZBCCUSTOM_CUSTOM_ZBC_H
+
+int custom_write(char *file, char *path);
+
+#endif //LIBZBCCUSTOM_CUSTOM_ZBC_H
